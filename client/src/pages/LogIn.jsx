5884ed1986ef7e6aa59d5b4a849efba1c7e2270c
mafarthi <maxfarthing1@gmail.com>
2024-08-19 20:59:10 -0400

    working on login functionality

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Box } from '@mui/material'
+import { Link, useNavigate } from 'react-router-dom'
+import { Box, Button } from '@mui/material'
 
 export default function LogInPage() {
-  return (
-    <Box sx={{ mt: 20 }}>
-        <form>
-            <label>Email</label>
-            <input />
-            <label>Password</label>
-            <input />
-        </form>
-        <Link to={'/login/create'}>Create an Account</Link>
-    </Box>
-  )
+    const navigate = useNavigate()
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const fd = new FormData(event.target)
+        const acqChannel = fd.getAll('acquisition')
+        const data = Object.fromEntries(fd.entries())
+        data.acquisition = acqChannel
+
+        const info = {
+            email: data.email,
+            password: data.password
+        }
+
+        fetch('http://localhost:5000/', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+            .then(result => result.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+
+        navigate('/')
+    }
+
+    return (
+        <Box sx={{ mt: 20 }}>
+            <form onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input id='email' type='email' name='email' required />
+                <label>Password</label>
+                <input id='password' type='password' name='password' required />
+                <Button type='submit'>Log in</Button>
+            </form>
+            <Link to={'/login/create'}>Create an Account</Link>
+        </Box>
+    )
 }
--- client/src/pages/NewAccount.jsx
+++ client/src/pages/NewAccount.jsx
@@ -1,16 +1,48 @@
 import { Box, Button } from '@mui/material'
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function NewAccountPage() {
-  return (
-    <Box sx={{ mt: 20 }}>
-        <label>Email</label>
-        <input />
-        <label>Password</label>
-        <input />
-        <label>Confirm Password</label>
-        <input />
-        <Button>Submit</Button>
-    </Box>
-  )
+    const navigate = useNavigate()
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const fd = new FormData(event.target)
+        const acqChannel = fd.getAll('acquisition')
+        const data = Object.fromEntries(fd.entries())
+        data.acquisition = acqChannel
+
+        let email = data.email
+        const info = {
+            email,
+            password: data.password
+        }
+
+        fetch('http://localhost:5000/auth/signup', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+
+        navigate('/login')
+    }
+
+    return (
+        <Box sx={{ mt: 20 }}>
+            <form onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input id='email' type='email' name='email' required />
+                <label>Password</label>
+                <input id='password' type='password' name='password' required />
+                <label>Confirm Password</label>
+                <input />
+                <Button type='submit'>Submit</Button>
+            </form>
+        </Box>
+    )
 }
